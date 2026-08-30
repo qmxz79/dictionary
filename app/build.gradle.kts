4b0ceddf9b59b1db16565292da3291af2ea59439
qmxz79 <qmxz79@gmail.com>
2026-08-30 20:41:10 +0800

    chore: keep release version as 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.bilingual.dictionary"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 1
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
